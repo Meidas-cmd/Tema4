a93fcd02f70a59decb871ef573653b77ec2b535e
A705\JorgeC.G. <jorgecuartero29@gmail.com>
2025-12-15 20:51:51 +0100

    no message

--- src/main/java/org/example/Proyecto_Bonito.java
+++ src/main/java/org/example/Proyecto_Bonito.java
@@ -1,11 +1,7 @@
 package org.example;
 
 public class Proyecto_Bonito {
-//En este proyecto hay que añadir javadoc, metodos , comentar y explicar el codigo
-//Estructura del javadoc author version para que sirve
-//Los metodos que usare seran los de entrada de datos con comprobacion
-//Otro para introducir los datos en la matriz con comprobacion del tipo de formato
-//Metodo para leer los vecinos de cada posicion
+
 
     static java.util.Scanner entrada;
     public static void main(String[] args){
